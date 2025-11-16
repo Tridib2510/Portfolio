9c041a82dcfc1c9462b42cf7040bb73a769f5662
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-11-16 23:55:11 +0530

    Work

--- portfolio/src/components/Work.tsx
+++ portfolio/src/components/Work.tsx
@@ -145,7 +145,7 @@ const Work = ({isDarkMode}:{isDarkMode:boolean}) => {
                       className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold cursor-pointer"
                     >
                       <Link href={project.github}>
-                      <Image src={assets.github_logo}  alt="GitHub icon" className="w-5" />
+                     GitHub
                       </Link>
                     </CardItem>
                   </div>
