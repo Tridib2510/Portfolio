596250206efd68edc4a95d0ac181e0a15fa7cd59
Tridib Roy Chowdhury <162015558+Tridib2510@users.noreply.github.com>
2026-05-03 12:03:08 +0530

    Update project details for Intellex

--- portfolio/src/assets/assets.js
+++ portfolio/src/assets/assets.js
@@ -137,8 +137,8 @@ export const workData = [
     {
         title:'Intellex',
         description:'Built a Perplexity-style AI search engine using Tavily and Supabase that retrieves real-time web results and generates concise, context-aware answers using an LLM.',
-        bgImage:'/work-53.png',
-        dates:'February 2026',
+        bgImage:'/work-84.png',
+        dates:'March 2026',
         technologies:["Bun", "Typescript", "React","Langchain","Tavily","Supabase"],
         github:'https://github.com/Tridib2510/Perplexity-Clone',
         link:'https://perplexity-clone-nu-two.vercel.app/',
